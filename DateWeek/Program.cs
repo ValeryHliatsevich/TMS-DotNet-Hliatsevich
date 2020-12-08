8a6c7a005507d43db30facaf36133dd2c26f679b
ValeryHliatsevich <valery.hliatsevich@gmail.com>
2020-12-08 21:43:42 +0300

    init:add solution

---
+++ DateWeek/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DateWeek
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Enter the date to establish day of week with YYYY-MM-DD form: ");
+            try
+            {
+                var date = Convert.ToDateTime(Console.ReadLine());
+                var weekday = (date.DayOfWeek);
+                Console.WriteLine("Day of Week: ", weekday);
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Error form");
+                Main();
+            }
+
+
+        }
+    }
+}
